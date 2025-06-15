17fdefd3377401ab38df425be8bad9931020dae7
Antony Hyson Seltran <102172525+Antonyhyson@users.noreply.github.com>
2025-06-15 23:06:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/antonyhysonseltran-portfolio/', // <--- ADD OR UPDATE THIS LINE
+  base: '/Antonyhysonseltran-portfolio/', // <--- ADD OR UPDATE THIS LINE
   plugins: [react()],
   server: {
     host: true, // This allows the server to be accessed from network interfaces
